6ef5e97f9127301617e01ad101bfd29ec52e6391
Alex Hudenko <ahudenko@gmail.com>
2010-10-16 02:27:13 +0300

    group by date in view new styles some js fixes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
   $(':date').dateinput({format: 'yyyy-mm-dd'});
   $('#add_request').click(function(e){
+    e.preventDefault();
     $('.form').show();
     $('#cancel_request').show();
     $(this).hide();
   });
   $('#cancel_request').click(function(e){
+    e.preventDefault();
     $('.form').hide();
     $(this).hide();
     $('#add_request').show();
@@ -15,11 +17,16 @@ $(document).ready(function(){
     e.preventDefault();
     if (confirm('Точно поедете?')) {
       var tr = $(this).parents('tr');
+      var link = $(this);
       $.ajax({
         url: this.href,
         type: 'PUT',
         success: function(str){
           tr.find('span.empty_count').text(str);
+          if (str == '0') {
+            tr.addClass('disabled');
+            link.hide();
+          }
         }
       });
     }
